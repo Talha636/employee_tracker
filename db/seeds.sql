aa43d7cbfd09264d6f44917964996386c6949b49
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-11-11 19:38:57 -0500

    seeds sql complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,11 +5,22 @@ VALUES ("Web Development"),
        ("Sales");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("", ,),
-       ("", ,),
-       ("", ,),
-       ("", ,),
-       ("", ,),
-       ("", ,),
-       ("", ,),
-       ("", ,);
+VALUES ("Front End Developer", 80000, 1),
+       ("Back End Developer", 100000, 1),
+       ("Resource Manager", 75000, 2),
+       ("Hiring Manager", 70000, 2),
+       ("Accountant", 90000, 3),
+       ("Analyst", 95000, 3),
+       ("Sales Rep", 60000, 4),
+       ("Sales Manager", 85000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("El", "Chapo", 1, 2),
+       ("El", "Mayo", 2, NULL),
+       ("El", "Azul", 3, NULL),
+       ("Amado", "Carrillo", 4, 3),
+       ("Pacho", "Herrera", 5, 6),
+       ("Benjamin", "Arellano", 6, NULL),
+       ("Ramon", "Arellano", 7, 8),
+       ("Pablo", "Acosta", 8, NULL);
+    --    I was watching narcos prior to this^^
